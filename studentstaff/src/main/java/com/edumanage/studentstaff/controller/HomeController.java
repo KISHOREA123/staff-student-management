fccb1fd7279d6569f546e5b4f7dfb566ff724813
KISHOREA123 <kishore469312@gmail.com>
2025-12-22 21:47:09 +0530

    Added admin entity

---
+++ studentstaff/src/main/java/com/edumanage/studentstaff/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.edumanage.studentstaff.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToDashboard() {
+        return "redirect:/dashboard";
+    }
+}
---
+++ studentstaff/src/main/java/com/edumanage/studentstaff/entity/Admin.java
@@ -0,0 +1,24 @@
+package com.edumanage.studentstaff.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    // getters & setters
+}
