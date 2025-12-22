4939f4db4fa67356b096b968587c576e03195e55
KISHOREA123 <kishore469312@gmail.com>
2025-12-22 14:30:56 +0530

    implemented service layer

---
+++ Backend/studentstaff/studentstaff/src/main/java/com/edumanage/studentstaff/service/StaffService.java
@@ -0,0 +1,15 @@
+package com.edumanage.studentstaff.service;
+
+import java.util.List;
+import com.edumanage.studentstaff.entity.Staff;
+
+public interface StaffService {
+
+    Staff saveStaff(Staff staff);
+
+    List<Staff> getAllStaff();
+
+    Staff getStaffById(Long id);
+
+    void deleteStaff(Long id);
+}
---
+++ Backend/studentstaff/studentstaff/src/main/java/com/edumanage/studentstaff/service/StudentService.java
@@ -0,0 +1,15 @@
+package com.edumanage.studentstaff.service;
+
+import java.util.List;
+import com.edumanage.studentstaff.entity.Student;
+
+public interface StudentService {
+
+    Student saveStudent(Student student);
+
+    List<Student> getAllStudents();
+
+    Student getStudentById(Long id);
+
+    void deleteStudent(Long id);
+}
